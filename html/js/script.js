35b67b9990c773dd9a751f21204fcfe635770db2
rahul <rahul_r156@yahoo.com>
2015-01-12 00:05:42 +0530

    first commit
    Updated js , css and images

---
+++ html/js/script.js
@@ -0,0 +1,47 @@
+// JavaScript Document
+
+$( window ).resize(function() { /*resize function makes sure of resolution of device*/   
+
+
+	var leftHeight = $('.metro_tile_top').height() + $('.metro_tile_bottom').height();
+    var leftHeight1=leftHeight + 29;
+    $('.metro_tile_right').css({'height':leftHeight1});
+	
+	var rightHeight_new = $('.metro_tile_last:first-child').height();
+    $('.metro_tile_last').css({'height':rightHeight_new});
+						
+});
+$(document).ready(function() {
+  
+    var leftHeight = $('.metro_tile_top').height() + $('.metro_tile_bottom').height();/*function makes sure on document when ready*/   
+    var leftHeight1=leftHeight + 29;
+    $('.metro_tile_right').css({'height':leftHeight1});
+	
+	var rightHeight_new = $('.metro_tile_last:first-child').height();
+    $('.metro_tile_last').css({'height':rightHeight_new});
+	
+	$('.grid_mouse').hover(function(){					   
+		$('.grid_hover').show();	
+	
+	});
+	$( ".grid_mouse" ).hover(function() {
+		$( this ).addClass( "hover" );
+  	});
+	$( ".navbar-nav li" ).click(function() {
+		$('ul li.active').removeClass('active');
+    	$(this).closest('li').addClass('active');
+  	});
+		
+});
+/*nav bar*/
+$(document).scroll(function() {    
+	var scroll = $(this).scrollTop();
+	if (scroll >= $('.navbar').height()) {
+		setTimeout('$(".navbar").addClass("navbar-fixed-top navbar-carbon-translucent")');
+	}
+	else 
+	{
+		setTimeout('$(".navbar").removeClass("navbar-fixed-top navbar-carbon-translucent")');
+	}
+});
+/*nav bar over*/
